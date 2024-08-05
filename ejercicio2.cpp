d5b1d2aadc67f4f342f127dbacc10894a00ac712
arramosm13 <arramosm@unjbg.edu.pe>
2024-08-05 10:13:42 -0500

    Inicie el programa colocando los datos de los atletas.

---
+++ ejercicio2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int i;
+	const string nombres_atletas=0;
+	nombres_atletas [6];{
+		"Carl Lewis"; "Marit Mjorgen"; "isabell Werth"; 
+		"Katie Ledecky"; "Michael Phelps"; "Birgit Fischer";
+	}
+	cout<<"Los atletas son: "<< i+1<< ".- "; nombres_atletas>>endl;
+	const string pais_atletas=0;
+	pais_atletas[6];{
+		"EEUU"; "Noruega"; "Alemania"; 
+		"EEUU"; "EEUU"; "Alemania";
+	}
+	return 0;
+}
