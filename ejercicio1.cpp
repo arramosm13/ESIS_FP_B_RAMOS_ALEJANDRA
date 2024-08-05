933d92d5926c24cebefd975a0874c5445f894eed
arramosm13 <arramosm@unjbg.edu.pe>
2024-08-05 10:27:01 -0500

    comence hacer los if para que la matriz salga como lo pedido.

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -2,7 +2,39 @@
 using namespace std;
 
 int main (){
-	int n;
-	cout<<"Ingrese el tamano de la matriz";cin>>n;
+	int n, p;
+	cout<<"Ingrese el tamano de la matriz: ";cin>>n;
+	int A[100][100];
+
+	
+	for (int i=0; i<n; i++){
+		for (int j=0; j< n; j++){
+		
+		A[i][j]=1;
+	}
+	}
+	for (int i=0; i<n; i++){
+		for (int j=0; j< n; j++)
+		if (i=j){
+			A[i][j]=1;
+		}
+	}
+	p=3*n;
+	for (int i=0; i<p; i++){
+		for (int j=0; j< p; j++){
+		
+		if (i<j)
+		A[i][j]=1;
+		}
+	}
+	for (int i=0; i<p; i++){
+		for (int j=0; j< p; j++){
+		
+		if (i<j)
+		A[i][j]=1;
+		}
+	}
+	//profesor considere que el internete staba muy malo al incio, por favor.
+	 
 return 0;	
 }
